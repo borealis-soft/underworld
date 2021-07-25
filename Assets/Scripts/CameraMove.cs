efdb92ee20ff2d7ab22015a766bebe0c254278a8
Twixuss <Twixuss@yandex.ru>
2021-07-25 22:33:01 +0300

    Dofiga!

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -4,30 +4,49 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    public float Speed;
-    public float ZoomSpeed;
-    public KeyCode UpKey = KeyCode.W;
-    public KeyCode DownKey = KeyCode.S;
-    public KeyCode LeftKey = KeyCode.A;
-    public KeyCode RightKey = KeyCode.D;
+	public float Speed;
+	public float ZoomSpeed;
 
-    private Vector3 lastPos;
+	public KeyCode UpKey    = KeyCode.W;
+	public KeyCode DownKey  = KeyCode.S;
+	public KeyCode LeftKey  = KeyCode.A;
+	public KeyCode RightKey = KeyCode.D;
 
-    void Update()
-    {
-        if (Input.GetKey(UpKey))
-            transform.Translate(Speed * Time.deltaTime * -transform.forward);
-        if (Input.GetKey(DownKey))
-            transform.Translate(Speed * Time.deltaTime * transform.forward);
-        if (Input.GetKey(LeftKey))
-            transform.Translate(Speed * Time.deltaTime * transform.right);
-        if (Input.GetKey(RightKey))
-            transform.Translate(Speed * Time.deltaTime * -transform.right);
+	[SerializeField] private float minZoom = 0;
+	[SerializeField] private float maxZoom = 5;
+	[SerializeField] private float minX    = 0;
+	[SerializeField] private float maxX    = 16;
+	[SerializeField] private float minZ    = 0;
+	[SerializeField] private float maxZ    = 23;
 
-        float scrollDelta = Input.GetAxis("Mouse ScrollWheel");
-        if (scrollDelta > 0.1)
-            transform.position += transform.forward * ZoomSpeed * Time.deltaTime;
-        if (scrollDelta < -0.1)
-            transform.position -= transform.forward * ZoomSpeed * Time.deltaTime;
-    }
+	private float currentZoom;
+	private Vector3 topPosition;
+	private Quaternion moveRotation;
+
+	private void Start()
+	{
+		topPosition = transform.position;
+		moveRotation = Quaternion.Euler(
+			0,
+			transform.eulerAngles.y,
+			transform.eulerAngles.z
+		);
+	}
+	void Update()
+	{
+		Vector3 moveDelta = default;
+		if (Input.GetKey(UpKey)) moveDelta.z += Speed * Time.deltaTime;
+		if (Input.GetKey(DownKey)) moveDelta.z -= Speed * Time.deltaTime;
+		if (Input.GetKey(RightKey)) moveDelta.x += Speed * Time.deltaTime;
+		if (Input.GetKey(LeftKey)) moveDelta.x -= Speed * Time.deltaTime;
+		topPosition += moveRotation * moveDelta;
+		topPosition.x = Mathf.Clamp(topPosition.x, minX, maxX);
+		topPosition.z = Mathf.Clamp(topPosition.z, minZ, maxZ);
+
+		currentZoom = Mathf.Clamp(currentZoom + Input.GetAxis("Mouse ScrollWheel") * ZoomSpeed, minZoom, maxZoom);
+
+		transform.position = topPosition + transform.forward * currentZoom;
+
+		Debug.DrawLine(topPosition, transform.position, Color.red);
+	}
 }
--- Assets/Scripts/LevelEditor.cs
+++ Assets/Scripts/LevelEditor.cs
@@ -7,21 +7,22 @@ using UnityEditor;
 public class LevelEditor : MonoBehaviour
 {
 	[HideInInspector] public GameObject Parent;
-	public GameObject cellPrefab;
+	public GameObject CellPrefab;
+	public GameObject RoadPrefab;
 	public int Width = 15, Length = 15;
+	public Texture2D PixelMap;
 	// Start is called before the first frame update
 	void Start()
 	{
-		
+
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		
+
 	}
 }
-
 
 [CustomEditor(typeof(LevelEditor))]
 public class LevelEditorEditor : Editor
@@ -42,15 +43,19 @@ public class LevelEditorEditor : Editor
 				editor.Parent.name = "Tiles";
 			}
 
-			
+
 
 			Vector3 InstPos = new Vector3(0.5f, 0, 0.5f);
-			for (int i = 0; i < editor.Width; i++)
+			for (int y = 0; y < editor.Length; y++)
 			{
-				for (int j = 0; j < editor.Length; j++)
+				for (int x = 0; x < editor.Width; x++, InstPos.x += 1)
 				{
-					Instantiate(editor.cellPrefab, InstPos, Quaternion.identity, editor.Parent.transform);
-					InstPos.x += 1;
+					Color pixelColor = editor.PixelMap.GetPixel(x, y);
+					if (pixelColor != Color.blue)
+					{
+						GameObject newObj = pixelColor == Color.black ? editor.RoadPrefab : editor.CellPrefab;
+						Instantiate(newObj, InstPos, Quaternion.Euler(pixelColor == Color.black ? -90 : 0, 0, 0), editor.Parent.transform);
+					}
 				}
 				InstPos.z += 1;
 				InstPos.x = 0.5f;
