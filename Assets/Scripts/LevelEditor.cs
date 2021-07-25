0eff10cfffab94477f29896ba61b9f3232d37407
Тимонин Виталий <Twixuss@yandex.ru>
2021-07-25 16:02:37 +0300

    add code in LevelEditor.cs

--- Assets/Scripts/LevelEditor.cs
+++ Assets/Scripts/LevelEditor.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+
 
 public class LevelEditor : MonoBehaviour
 {
@@ -14,5 +16,34 @@ public class LevelEditor : MonoBehaviour
     void Update()
     {
         
+    }
+}
+
+
+[CustomEditor(typeof(LevelEditor))]
+public class LevelEditorEditor : Editor
+{
+    public GameObject cellPrefab;
+    public int Width = 15, Length = 15;
+
+    public override void OnInspectorGUI()
+	{
+        base.OnInspectorGUI();
+
+        var editor = target as LevelEditor;
+
+        if (GUILayout.Button("Generate"))
+		{
+            Vector3 InstPos = Vector3.zero;
+            for (int i = 0; i < Width; i++)
+			{
+                for (int j = 0; j < Length; j++)
+				{
+                    Instantiate(cellPrefab, InstPos, Quaternion.identity);
+                    InstPos += Vector3.right;
+				}
+                InstPos += Vector3.forward;
+			}
+		}
     }
 }
